9c49131628b93da8e13d074f1167d9de15fa7d7d
lililiyf <85494471+lililiyf@users.noreply.github.com>
2021-06-08 17:52:29 +0800

    Delete chem directory

--- chem/sparse.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-graph_adj = torch.sparse_coo_tensor(indices=dataset[i].graph_adj.clone().detach().t(),
-                                    values=torch.ones((len(dataset[i].graph_adj)), dtype=torch.int),
-                                    size=(dataset[i].num_nodes, dataset[i].num_nodes))
-
-graph_adj.
